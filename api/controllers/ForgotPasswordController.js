e3dd168035f9896023de3479c7651f7cc377d86b
kubop <pernica.jakub@gmail.com>
2020-05-17 00:21:51 +0200

    forgotPassword basic functionality

---
+++ api/controllers/ForgotPasswordController.js
@@ -0,0 +1,41 @@
+const citatelia = [
+    { 
+        id: 1,
+        meno: 'Jakub',
+        priezvisko: 'Pernica',
+        email: 'pernica.jakub@gmail.com',
+        telefonne_cislo: null,
+        heslo: 'moje_heslo',
+        typ_oznamenia: 1
+    },
+    { 
+        id: 2,
+        meno: 'Patrik',
+        priezvisko: 'Tománek',
+        email: 'patrikwet@gmail.com',
+        telefonne_cislo: null,
+        heslo: 'lol',
+        typ_oznamenia: 1
+    }
+]
+
+exports.forgotPassword = function(req, res) {
+    const body = req.body
+
+    if (!body || !body.email) {
+        return res.status(400).json({ error: 'Bad request' }) 
+    } 
+
+    const citatel = citatelia.find(c => c.email === body.email)
+    if (!citatel) {
+        return res.status(200).json({ error: 'User with specified email not found' })
+    }
+
+    const heslo = citatel.heslo // toto heslo mu chceme poslat na email, asi cez webovu sluzbu stranky
+
+    /**
+     * Tu bude kod, ktory cez webovu sluzbu PIS posle na email heslo
+     */
+
+    return res.status(200).json({ password: heslo }) // Zatial len odosielame heslo ako response, tu bude potom napríklad: { success: true}, to bude znamenat ze email bol uspesne odoslany
+}
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 
 const VypozickyController = require('./api/controllers/VypozickyController.js')
 const CitateliaController = require('./api/controllers/CitateliaController.js')
+const ForgotPasswordController = require('./api/controllers/ForgotPasswordController.js')
 
 const app = express()
 const port = 8081
@@ -10,9 +11,14 @@ const port = 8081
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+// VypozickyController
 app.get('/vypozicky', VypozickyController.getAll)
 app.post('/vypozicky', VypozickyController.postRequest)
 
+// CitateliaController
 app.get('/citatelia', CitateliaController.getAll)
+
+// ForgotPasswordController
+app.post('/forgotPassword', ForgotPasswordController.forgotPassword)
 
 app.listen(port, () => console.log(`Backend running on localhost:${port}`))
