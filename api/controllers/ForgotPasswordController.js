b7b3b48d5383d1a5a6157ffb31c7f89422b85f90
Jakub Pernica <pernica.jakub@gmail.com>
2020-05-17 21:35:18 +0200

    komenty

--- api/controllers/ForgotPasswordController.js
+++ api/controllers/ForgotPasswordController.js
@@ -33,20 +33,24 @@ exports.verifyEmailAddress = function(req, res) {
 exports.forgotPassword = function(req, res) {
     const body = req.body
 
+    // Ak v parametroch neprisiel email => zly request
     if (!body || !body.email) {
         return res.status(400).json({ error: 'Bad request' }) 
     } 
 
+    // Nacitame si všetkých čitateľov
     fs.readFile('./data/citatelia.json', 'utf8', function (err, data) {
-        if (err) {
+        if (err) { // Len nejaky error handling
             console.log(err)
             return res.status(500).json({ error: 'Internal server error' }) 
         }
 
+        // načítame si čitateľov vo formáte JSON
         const citatelia = JSON.parse(data)
         
+        // Nájdeme čitateľa, ktorého email sa zhodnuje s emailom ktorý je v requeste
         const citatel = citatelia.find(c => c.email === body.email)
-        if (!citatel) {
+        if (!citatel) { // Ak sme takéto nenašli => erreor
             return res.status(200).json({ error: 'Vami zadaná emailová adresa nie je registrovaná v našom systéme' })
         }
     
@@ -56,7 +60,6 @@ exports.forgotPassword = function(req, res) {
          * Tu bude kod, ktory cez webovu sluzbu PIS posle na email heslo
          * EDIT: kurva zistil som ze ta webova sluzba Email na PIS stranke je nejaka kktina čo posiela email tímu alebo čo
          */
-    
     
         return res.status(200).json({ password: heslo }) // Zatial len odosielame heslo ako response, tu bude potom napríklad: { success: true}, to bude znamenat ze email bol uspesne odoslany
     })
