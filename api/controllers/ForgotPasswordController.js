18a92efc3afe6854f72008cf7e7c96fd8ff0e067
kubop <pernica.jakub@gmail.com>
2020-05-17 11:30:11 +0200

    fix

--- api/controllers/ForgotPasswordController.js
+++ api/controllers/ForgotPasswordController.js
@@ -21,6 +21,29 @@ const citatelia = [
     }
 ]
 
+var validateEmail = function(email, callback) {
+    var url = 'http://pis.predmety.fiit.stuba.sk/pis/ws/Validator?WSDL'
+    var args = { email: email }
+
+    soap.createClient(url, function(err, client) {
+        if (err) {
+            return callback(err)
+        }
+
+        client.validateEmail(args, function(err, result) {
+            if (err) {
+                return callback(err)
+            }
+
+            if (result.success === true) {
+                return callback(null)
+            } else {
+                return callback('Invalid email format')
+            }
+        })
+    })
+}
+
 exports.forgotPassword = function(req, res) {
     const body = req.body
 
@@ -28,26 +51,26 @@ exports.forgotPassword = function(req, res) {
         return res.status(400).json({ error: 'Bad request' }) 
     } 
 
-    var url = 'http://pis.predmety.fiit.stuba.sk/pis/ws/Validator?WSDL'
-    var args = { email: body.email }
-    soap.createClient(url, function(err, client) {
-        client.validateEmail(args, function(err, result) {
-            console.log(result)
-        })
-    })
-
-    const citatel = citatelia.find(c => c.email === body.email)
-    if (!citatel) {
-        return res.status(200).json({ error: 'User with specified email not found' })
-    }
-
-    const heslo = citatel.heslo // toto heslo mu chceme poslat na email, asi cez webovu sluzbu stranky
-
-    /**
-     * Tu bude kod, ktory cez webovu sluzbu PIS posle na email heslo
-     * EDIT: kurva zistil som ze ta webova sluzba Email na PIS stranke je nejaka kktina čo posiela email tímu alebo čo
-     */
-
-
-    return res.status(200).json({ password: heslo }) // Zatial len odosielame heslo ako response, tu bude potom napríklad: { success: true}, to bude znamenat ze email bol uspesne odoslany
+    validateEmail(body.email, (err) => {
+        if (err) {
+            console.log(err)
+            return res.status(400).json({ error: err })
+        } else {
+            const citatel = citatelia.find(c => c.email === body.email)
+            if (!citatel) {
+                return res.status(200).json({ error: 'User with specified email not found' })
+            }
+        
+            const heslo = citatel.heslo // toto heslo mu chceme poslat na email, asi cez webovu sluzbu stranky
+        
+            /**
+             * Tu bude kod, ktory cez webovu sluzbu PIS posle na email heslo
+             * EDIT: kurva zistil som ze ta webova sluzba Email na PIS stranke je nejaka kktina čo posiela email tímu alebo čo
+             */
+        
+        
+            return res.status(200).json({ password: heslo }) // Zatial len odosielame heslo ako response, tu bude potom napríklad: { success: true}, to bude znamenat ze email bol uspesne odoslany
+        }
+    })    
 }
+
